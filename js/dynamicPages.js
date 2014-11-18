b350ed254d0f99b03f92a651cd7f96526082ce2c
Jens Trio <jens@jnstr.be>
2014-11-18 09:55:39 +0100

    moved js for dynamic loading pages to separate file

---
+++ js/dynamicPages.js
@@ -0,0 +1,39 @@
+var dynamicPages = (function() {
+    var defaultContent = '';
+    init = function(){
+        defaultContent = ($('#content').html());
+        hash = window.location.hash.substring(1);
+        if (hash != '') loadPage(hash);
+        else history.pushState('home','null','');
+        initOnclick();
+        initPopstate();
+    };
+    var initPopstate = function() {
+        $(window).on('popstate', function(e) {
+            if (e.originalEvent.state && e.originalEvent.state) {
+                loadPage(e.originalEvent.state,false)
+            }
+        });
+    };
+    var initOnclick = function() {
+        $('#content').on('click', 'a.load', function(e) {
+            e.preventDefault();
+            loadPage($(this).attr('href'));
+        });
+    };
+    var loadPage = function(page, push) {
+        if(typeof(push)==='undefined') push = true;
+        if (push == true) history.pushState(page ,'','#' + page);
+
+        $('#content').empty();
+
+        if (page == 'home') $('#content').append(defaultContent);
+        else $('#content').load(page + ".html");
+    };
+
+    return {
+        init:init,
+        loadPage:loadPage
+    };
+})();
+
