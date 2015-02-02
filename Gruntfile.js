a66621c829c438e1892a43fff6d63eb4cdec4dab
Tom Van Herreweghe <tom@king-foo.be>
2015-02-02 22:08:02 +0100

    Grunt - Added task to copy dist files of dependencies
    Signed-off-by: Tom Van Herreweghe <tom@king-foo.be>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
 	// load dependencies
-	grunt.loadNpmTasks('grunt-git');
+	grunt.loadNpmTasks("grunt-git");
 
 	// Project configuration.
 	grunt.initConfig({
@@ -10,11 +10,31 @@ module.exports = function(grunt) {
 					branch: "gh-pages"
 				}
 			}
+		},
+		generate: {
+			files: {
+				// src: dest
+				'js/vendor/bootstrap/dist/js/bootstrap.min.js': 'js/bootstrap.min.js',
+				'js/vendor/handlebars/handlebars.min.js': 'js/handlebars.min.js',
+				'js/vendor/jquery/dist/jquery.min.js': 'js/jquery.min.js',
+				'js/vendor/modernizr/modernizr.js': 'js/modernizr.js'
+			}
 		}
 	});
 
 
 	// deploy
-	grunt.registerTask('deploy', ['gitcheckout:ghpages']);
+	grunt.registerTask("deploy", ["gitcheckout:ghpages", "generate"]);
+
+	grunt.registerTask("generate", function(){
+		grunt.log.writeln('Generating files...');
+
+		var files = grunt.config.get(this.name + '.files');
+		for (var src in files) {
+			var dest = files[src];
+			grunt.file.copy(src, dest);
+			grunt.log.writeln("Copying [" + src + "] to [" + dest + "]").ok();
+		}
+	});
 
 };
