1f9dd798581ec095a6f558fd9386b990148cf46b
Jens Trio <jens.trio@gmail.com>
2015-02-14 17:58:13 +0100

    javascript pages loader update

--- js/dynamicPages.js
+++ js/dynamicPages.js
@@ -1,29 +1,32 @@
-var dynamicPages = (function() {
+var dynamicPages = (function () {
     var defaultContent = '';
-    init = function(){
+
+    var init = function () {
         defaultContent = ($('#content').html());
         hash = window.location.hash.substring(1);
         if (hash != '') loadPage(hash);
-        else history.pushState('home','null','');
+        else history.pushState('home', 'null', '');
         initOnclick();
         initPopstate();
     };
-    var initPopstate = function() {
-        $(window).on('popstate', function(e) {
+
+    var initPopstate = function () {
+        $(window).on('popstate', function (e) {
             if (e.originalEvent.state && e.originalEvent.state) {
-                loadPage(e.originalEvent.state,false)
+                loadPage(e.originalEvent.state, false)
             }
         });
     };
-    var initOnclick = function() {
-        $('#content').on('click', 'a.load', function(e) {
+
+    var initOnclick = function () {
+        $('#content').on('click', 'a.load', function (e) {
             e.preventDefault();
             loadPage($(this).attr('href'));
         });
     };
-    var loadPage = function(page, push) {
-        if(typeof(push)==='undefined') push = true;
-        if (push == true) history.pushState(page ,'','#' + page);
+    
+    var loadPage = function (page, push) {
+        if (push === true || push === undefined) history.pushState(page, '', '#' + page);
 
         $('#content').empty();
 
@@ -32,8 +35,8 @@ var dynamicPages = (function() {
     };
 
     return {
-        init:init,
-        loadPage:loadPage
+        init: init,
+        loadPage: loadPage
     };
 })();
 
