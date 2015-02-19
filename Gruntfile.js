8e6b7d3356d84e2d5a857ea0f6a06991ead12030
Tom Van Herreweghe <tom@king-foo.be>
2015-02-19 13:45:42 +0100

    Reverted changes to Gruntfile
    Signed-off-by: Tom Van Herreweghe <tom@king-foo.be>

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,6 +25,16 @@ module.exports = function(grunt) {
                 }
             }
         },
+		generate: {
+			files: {
+				// src: dest
+				'js/vendor/bootstrap/dist/js/bootstrap.min.js': 'js/bootstrap.min.js',
+				'js/vendor/handlebars/handlebars.min.js': 'js/handlebars.min.js',
+				'js/vendor/jquery/dist/jquery.min.js': 'js/jquery.min.js',
+				'js/vendor/modernizr/modernizr.js': 'js/modernizr.js',
+                'js/vendor/requirejs/require.js': 'js/require.js'
+			}
+		},
         gitadd: {
             all: {
                 options: {
@@ -55,9 +65,8 @@ module.exports = function(grunt) {
 
 
 	// deploy
-	grunt.registerTask("deploy", ["gitcheckout:ghpages", "gitmerge:master", "gitadd:all", "gitcommit:ghpages", "gitpush:origin", "gitcheckout:master"]);
+	grunt.registerTask("deploy", ["gitcheckout:ghpages", "gitmerge:master", "generate", "gitadd:all", "gitcommit:ghpages", "gitpush:origin", "gitcheckout:master"]);
 
-    /*
 	grunt.registerTask("generate", function(){
 		grunt.log.writeln('Generating files...');
 
@@ -70,6 +79,5 @@ module.exports = function(grunt) {
             }
 		}
 	});
-    */
 
 };
